7d39b3ecca528e4c5a35905fcccfacf71a099fb2
Gage Harmon <gaharmon1@gmail.com>
2023-01-31 09:21:26 -0700

    Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+   return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    
+}
+
+
+const leastFavoriteCustomer = 'Gage';
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'Random';
+}
